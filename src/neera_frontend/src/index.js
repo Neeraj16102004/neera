83e5917333f306f6d43d206f39e93307992d399b
Neeraj Rana <neerajrana@Neerajs-MacBook-Air.local>
2023-11-18 12:56:54 +0530

    Initial commit.

---
+++ src/neera_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { neera_backend } from "../../declarations/neera_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await neera_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
